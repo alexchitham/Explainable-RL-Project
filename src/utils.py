5baf3ad4d371cb821f6318f2c7498b461cea3d4b
Alex Chitham <alexchitham@hotmail.co.uk>
2025-03-25 17:40:17 +0000

    Add get_action_probabilities function to utils

--- src/utils.py
+++ src/utils.py
@@ -20,3 +20,23 @@ def get_q_values(agent, states):
         q_values = np.squeeze(q_values.numpy(), axis=0)
 
     return q_values
+
+
+def get_action_probabilities(agent, states, softmax=True):
+
+    # Get the q-values
+    q_values = get_q_values(agent, states)
+
+    # Use softmax so each batch of probabilities sums to 1
+    if softmax:
+        softmax = torch.nn.Softmax(dim=1)
+        action_probs = softmax(q_values)
+
+    # Use a one-hot style tensor to represent the most probable action
+    else: 
+        batch_size = q_values.shape[0]
+        most_probable_action = torch.argmax(q_values, 1)
+        action_probs = torch.zeros_like(q_values)
+        action_probs[torch.arange(batch_size), most_probable_action] = 1.0
+
+    return action_probs
